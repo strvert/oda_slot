0238fd08edffd086bfda3168fb46ca16fbbd2ac8
Okazaki Kan <ok16039@tomakomai.kosen-ac.jp>
2018-12-04 10:32:04 +0900

    難易度設定の実装,oda_slotへの環境設定

--- control_game.ino
+++ control_game.ino
@@ -1,1 +1,16 @@
+Difficulty getGameDifficulty() {
+  return diff;
+}
 
+void setDifficulty(Difficulty dif){
+  diff = dif;
+}
+
+void readDifficulty(){
+  if(get_sw_state(1))
+    setDifficulty(HARD);
+  else if(get_sw_state(0))
+    setDifficulty(NORMAL);
+  else
+    setDifficulty(EASY);
+}
